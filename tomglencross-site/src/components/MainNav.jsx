628623636241bd485930e9d0cb5f7652b62a974e
Tom Glencross <tomglencross@Toms-MacBook-Pro-2.local>
2025-02-15 22:48:00 +0000

    updates works list to be generated from test data

--- tomglencross-site/src/components/MainNav.jsx
+++ tomglencross-site/src/components/MainNav.jsx
@@ -5,11 +5,23 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export default function MainNav() {
+    const fakeWorksData = [
+        {id: "1", reference: "1.1", title: "The Old Stones", description: "a folklore exhibition", body: "this is some text about the LORE and the old stones."},
+        {id: "2", reference: "1.2", title: "Indebtedness", description: "an exhibition in Kevin Space", body: "this is some text about this exhibition."},
+        {id: "3", reference: "1.3", title: "Indelicate", description: "a photograph", body: "this is some text about Indelicate."}
+    ]
+
+    const worksListFunction = () => {
+        return fakeWorksData.map((work)=>{
+            return work.reference + " " + work.title
+        })
+    }
+    console.log(worksListFunction())
+
     const [isMenuOpen, setIsMenuOpen] = useState(true)
     const [expanded, setExpanded] = useState(null)
     const [selectedItem, setSelectedItem] = useState(null)
     const router = useRouter()
-console.log(selectedItem)
 
     const toggleSubMenu = (menu) => {
         setExpanded(expanded === menu ? null: menu)
@@ -25,7 +37,9 @@ console.log(selectedItem)
         } else {
         router.push(route)
         setSelectedItem(item)
-        const worksList = ['1.1 Work 1','1.2 Work 2','1.3 Work 3']
+        console.log(item)
+        const worksList = worksListFunction()
+        console.log(worksList)
         worksList.includes(item) ? setExpanded('works') : setExpanded(null)
         setIsMenuOpen(false)
         }
@@ -57,22 +71,22 @@ console.log(selectedItem)
               {expanded === 'works' && (
                 <ul className="pl-4 space-y-0">
                   <li
-                    onClick={() => handleItemClick('1.1 Work 1', '/')}
-                    className={`cursor-pointer ${selectedItem === '1.1 Work 1' ? 'text-pinkCustom' : ''} hover:text-pinkCustom transition-colors duration-300 `}
+                    onClick={() => handleItemClick('1.1 The Old Stones', '/')}
+                    className={`cursor-pointer ${selectedItem === '1.1 The Old Stones' ? 'text-pinkCustom' : ''} hover:text-pinkCustom transition-colors duration-300 `}
                   >
-                    <span className="text-black">1.1</span> Work 1
+                    <span className="text-black">1.1</span> The Old Stones
                   </li>
                   <li
-                    onClick={() => handleItemClick('1.2 Work 2', '/')}
-                    className={`cursor-pointer ${selectedItem === '1.2 Work 2' ? 'text-pinkCustom' : ''}hover:text-pinkCustom transition-colors duration-300 `}
+                    onClick={() => handleItemClick('1.2 Indebtedness', '/')}
+                    className={`cursor-pointer ${selectedItem === '1.2 Indebtedness' ? 'text-pinkCustom' : ''}hover:text-pinkCustom transition-colors duration-300 `}
                   >
-                    <span className="text-black">1.2</span> Work 2
+                    <span className="text-black">1.2</span> Indebtedness
                   </li>
                   <li
-                    onClick={() => handleItemClick('1.3 Work 3', '/')}
-                    className={`cursor-pointer ${selectedItem === '1.3 Work 3' ? 'text-pinkCustom' : ''}hover:text-pinkCustom transition-colors duration-300 `}
+                    onClick={() => handleItemClick('1.3 Indelicate', '/')}
+                    className={`cursor-pointer ${selectedItem === '1.3 Indelicate' ? 'text-pinkCustom' : ''}hover:text-pinkCustom transition-colors duration-300 `}
                   >
-                    <span className="text-black">1.3</span> Work 3
+                    <span className="text-black">1.3</span> Indelicate
                   </li>
                 </ul>
               )}
@@ -82,7 +96,7 @@ console.log(selectedItem)
             <li>
               <div
                 onClick={() => handleItemClick('2 Dev', 'dev')} 
-                className={`cursor-pointer ${selectedItem == '2 Dev' ? 'text-pinkCustom' : ''}hover:text-pinkCustom transition-colors duration-300 `}
+                className={`cursor-pointer ${selectedItem === '2 Dev' ? 'text-pinkCustom' : ''} hover:text-pinkCustom transition-colors duration-300 `}
               >
                 <span className="text-black">2</span> Dev
               </div>
@@ -90,7 +104,7 @@ console.log(selectedItem)
             <li>
               <div
                 onClick={() => handleItemClick('3 Blog', 'blog')}
-                className={`cursor-pointer ${selectedItem == '3 Blog' ? 'text-pinkCustom' : ''}hover:text-pinkCustom transition-colors duration-300 `}
+                className={`cursor-pointer ${selectedItem === '3 Blog' ? 'text-pinkCustom' : ''} hover:text-pinkCustom transition-colors duration-300 `}
               >
                 <span className="text-black">3</span> Blog
               </div>
@@ -98,7 +112,7 @@ console.log(selectedItem)
             <li>
               <div
                 onClick={() => handleItemClick('4 CV', 'cv')}
-                className={`cursor-pointer ${selectedItem == '4 CV' ? 'text-pinkCustom' : ''} hover:text-pinkCustom transition-colors duration-300 `}
+                className={`cursor-pointer ${selectedItem === '4 CV' ? 'text-pinkCustom' : ''} hover:text-pinkCustom transition-colors duration-300 `}
               >
                 <span className="text-black">4</span> CV
               </div>
@@ -137,5 +151,4 @@ console.log(selectedItem)
 -should menu disappear once selected? or remain in mobile too?
 -build other navbar
 -kanban rest of project...
--make selected items remain pink on menu return!! it only works for CV?!?!?!?
 */
