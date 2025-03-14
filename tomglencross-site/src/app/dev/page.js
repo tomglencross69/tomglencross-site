4c43d89f49c53caa0c2fe4ab4ef0018c69a4302a
tomglencross69 <thomasglencross@gmail.com>
2025-03-14 15:31:15 +0000

    completes basic devPage

--- tomglencross-site/src/app/dev/page.js
+++ tomglencross-site/src/app/dev/page.js
@@ -6,7 +6,7 @@ import { useRouter } from 'next/navigation';
 export default function Dev() {
   const router = useRouter()
   const [text, setText] = useState('');
-  const fullText = '2 Dev';
+  const fullText = '2 dev';
   const [index, setIndex] = useState(0);
   const [showCursor, setShowCursor] = useState(true);
   const [fadeIn, setFadeIn] = useState(false); 
@@ -41,14 +41,68 @@ export default function Dev() {
 
   return (
     <div 
-      className={`min-h-screen font-mono transition-opacity duration-[1000ms] ${fadeIn ? 'opacity-100' : 'opacity-0'}`}
+      className={`font-mono transition-opacity duration-[1000ms] ${fadeIn ? 'opacity-100' : 'opacity-0'}`}
     >
-      <h1 className="text-4xl font-bold text-green-400">
+      <h1 className="text-4xl text-green-400">
       <a className="cursor-pointer" onClick={() => {router.push("/")}}>↞ </a> 
       {text}
-      <span className={`ml-1 ${showCursor ? 'opacity-100' : 'opacity-0'}`}>|</span>
+      <span className={`ml-1 ${showCursor ? 'opacity-100' : 'opacity-0'}`}>▮</span>
       </h1>
-      <p className="mt-4 text-lg opacity-80 text-green-300">Welcome to my dev page.</p>
+      <p className="py-4 text-2xl text-green-300">Projects</p>
+      <div className='opacity-90 text-xl'>
+        NOCTURNE
+      </div>
+      <div>
+      <a 
+      className='underline cursor-pointer hover:text-pinkCustom hover:text-lg hover:underline transition-colors duration-1000'
+       href="https://nocturne-journal.netlify.app"
+      target="_blank"
+     >
+      nocturne-journal.netlify.app</a> 
+      <p>Nocturne is a playful way for users to interpret, remember and analyse their dreams with responsive and reactive visual and textual feedback provided by two custom-trained AI models.</p>
+      <p>- Full-stack web application built with React</p> 
+      <p>- Database and user account authentication in Firestore/Firebase</p> <p>- Deployed with Netlify</p> 
+      <p>- MotionFramer and tsParticles for dynamic visuals</p> 
+      <p>- Open-source LLMs hosted on Hugging Face</p> 
+      <p>- App demo and tech-stack can be viewed <a 
+      className='underline cursor-pointer hover:text-pinkCustom hover:text-lg hover:underline transition-colors duration-1000'
+      href="https://www.youtube.com/watch?v=eZsLUtNKPNk" 
+      target="_blank">
+      here</a></p> 
+      <p>- GitHub <a 
+      className='underline cursor-pointer hover:text-pinkCustom hover:text-lg hover:underline transition-colors duration-1000'
+      href="https://github.com/MGough22/dream-team"
+      target="_blank">
+      here</a></p>
+      </div>
+     <div className='pt-4 text-center'>+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+</div>
+      <div className='opacity-90 text-xl pt-4'>
+       TOMGLENCROSS.COM
+      </div>
+      <div className='pb-10'>
+      <a 
+      className='underline cursor-pointer hover:text-pinkCustom hover:text-lg hover:underline transition-colors duration-1000'
+       href="https:/tomglencross.com"
+      target="_blank"
+     >
+      tomglencross.com</a> 
+      <p>You're surfin' it right now buddy. A full-stack personal website project to host portfolio, projects, blog, artworks.</p>
+      <p>- Full-stack project built with Next.js & React using RESTful API principles</p> 
+      <p>- UI/UX designed following WCAG 3.0 accessibility, & low-energy guidelines</p>
+      <p>- Custom Admin Dashboard CMS with js-cookie login-auth, blogpost and comment approval/deletion </p> 
+      <p>- react-quill custom wysiwyg blogpost dashboard with live preview</p> 
+      <p>- Database built in PostgreSQL</p> 
+      <p>- TailwindCSS for dynamic inline styling and custom night-mode and dev-themes</p>  
+      <p>- Project plan and progress including wireframes, component tree etc., <a
+       className='underline cursor-pointer hover:text-pinkCustom hover:text-lg hover:underline transition-colors duration-1000'
+       href="https://www.figma.com/board/QLJFEnQSkpcz5B7RYu9ETN/tomglencross.com-project-board?node-id=0-1&p=f&t=lP7eonXJilk17Gzc-0"
+       target="_blank">here</a></p> 
+      <p>- GitHub <a 
+      className='underline cursor-pointer hover:text-pinkCustom hover:text-lg hover:underline transition-colors duration-1000'
+      href="https://github.com/tomglencross69/tomglencross-site"
+      target="_blank">
+      here</a></p>
+      </div>
     </div>
   );
 };
