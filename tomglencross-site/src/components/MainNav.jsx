eaafdbf3ced17050b45c7b179e20784ab489b747
tomglencross69 <thomasglencross@gmail.com>
2025-03-17 13:27:01 +0000

    media size menu rendering for /dev

--- tomglencross-site/src/components/MainNav.jsx
+++ tomglencross-site/src/components/MainNav.jsx
@@ -43,11 +43,16 @@ export default function MainNav() {
   // CALLS HANDLEITEMCLICK WHEN NAVIGATING TO DEV FROM CV
   useEffect(() => {
     if (prevPathname === "/cv/dev-cv" && pathname === "/dev") {
-      setIsMenuOpen(true);
-      toggleMode();
+      if (!isDarkMode) {
+        toggleMode();
+      }
+      const isScreenAbove768px = window.matchMedia("(min-width: 768px)").matches;
+      if (isScreenAbove768px) {
+        setIsMenuOpen(true);
+      }
     }
     setPrevPathname(pathname);
-  }, [pathname])
+  }, [pathname, prevPathname, isDarkMode]);
   
   const toggleSubMenu = (menu) => {
     setExpanded(expanded === menu ? null: menu)
