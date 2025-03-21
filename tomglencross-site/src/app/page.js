0fd1cb9df4c7e82df85b3621caa77db7195e3cb7
tomglencross69 <thomasglencross@gmail.com>
2025-03-21 21:28:16 +0000

    updates front page styling

--- tomglencross-site/src/app/page.js
+++ tomglencross-site/src/app/page.js
@@ -20,42 +20,31 @@ export default function Home() {
 
   return (
     <>
+  
       <div className="md:flex md:flex-col md:items-center md:pt-4">
         <div className="text-3xl md:text-4xl text-center">⚶</div>
         <div className="text-2xl md:text-4xl pt-3 pb-6 md:text-center">Tom Glencross is an artist, writer, and dev working in the UK.</div>
-        <div className="flex justify-center">
-          <Image 
-            alt="portrait of Tom Glencross beside some neolithic standing stones"
-            src={homepageImage}
-            width={300}
-            height={300}
-            className="intrinsic md:w-[400px] md:h-[auto]"
-            priority
-            placeholder="blur"
-          />
-        </div>
-        <div className="text-3xl md:text-4xl text-center pt-5 pb-5">߷</div>
-        <div className="border-t-[1px] border-black dark:border-white w-full text-center pb-4"></div>
-      </div>
-  <div className="ml-4">
-      {/* The red box with background behind it */}
-      <div className="mb-5 inline-block relative z-10">
-        {/* Background behind the box */}
+     
+        <div className="ml-4 text-center md:mt-4">
+      
+      <div className="mb-5 inline-block relative z-10 ">
+
         <div className="absolute bg-pinkCustom dark:bg-nightModeBlueCustom w-[101%] h-[98%] z-0"></div>
   
-        {/* The lifted red box */}
+       
         <div
           className="inline-block 
-           bg-[#FFF5FE;] dark:bg-black transform hover:scale-105 transition-transform duration-300 ease-in-out z-10"
+           bg-[#FFF5FE;] dark:bg-black transform hover:scale-105 transition-transform duration-300 ease-in-out z-10 "
+           
         >
-          <div className="text-2xl md:text-4xl text-pinkCustom bg-gradient-to-r from-pinkCustom via-nightModeBlueCustom to-pinkCustom bg-[length:200%_100%] animate-gradient bg-clip-text text-transparent ">
+          <div className="text-2xl md:text-4xl text-pinkCustom bg-gradient-to-r from-pinkCustom via-nightModeBlueCustom to-pinkCustom bg-[length:200%_100%] animate-gradient bg-clip-text text-transparent  ">
             <div>✿･.｡.: <i>LATEST POST</i>.:｡.･✿</div>
           </div>
   
           {blogPosts.length > 0 && (
             <div className=" w-full flex">
-              {/* Image Container (Left Side) */}
-              <div className="flex items-stretch">
+             
+              <div className="flex items-stretch ">
                 <Link href={`/blog/${blogPosts[0].blog_id}`} passHref>
                   <img
                     src={`${blogPosts[0].image_src}`}
@@ -65,9 +54,8 @@ export default function Home() {
                   />
                 </Link>
               </div>
-  
-              {/* Blue Box (Inside Yellow, Aligned to Right) */}
-              <div className={`flex-1 p-1 cursor-pointer hover:text-pinkCustom dark:nightModePinkCustom transition-colors duration-300`}>
+
+              <div className={`flex-1 p-1 text-left cursor-pointer hover:text-pinkCustom dark:nightModePinkCustom transition-colors duration-300`}>
                 <Link href={`/blog/${blogPosts[0].blog_id}`} passHref>
                   <div className="text-2xl md:text-3xl">{blogPosts[0].title}</div>
                   <div className="text-xl md:text-2xl">{blogPosts[0].subtitle}</div>
@@ -91,48 +79,24 @@ export default function Home() {
         </div>
       </div>
       </div>
+    
+        <div className="flex justify-center pl-3">
+          <Image 
+            alt="portrait of Tom Glencross beside some neolithic standing stones"
+            src={homepageImage}
+            width={300}
+            height={300}
+            className="intrinsic md:w-[400px] md:h-[auto] align-center "
+            priority
+            placeholder="blur"
+          />
+        </div>
+        <div className="text-3xl md:text-4xl text-center pt-5 pb-5">߷</div>
+        <div className="border-t-[1px] border-black dark:border-white w-full text-center pb-4"></div>
+        <div className="text-xs text-center pb-5"> © MMXXV TOM GLENCROSS. REFULGET LABORES NOSTROS COELUM </div>
+       
+      </div>
     </>
   );
 }
 
-/*
-<Link href={`/blog/${blogPost.blog_id}`} passHref>
-        <div
-          className={`${
-            isFirst ? "md:w-full md:h-[300px] md:flex md:items-start md:overflow-hidden ": " md:w-full md:h-[100px]"
-          }`}
-        >
-          <img
-            alt={blogPost.image_alt_text}
-            src={
-              blogPost.image_src.startsWith("http")
-                ? blogPost.image_src
-                : `/images/${blogPost.image_src.replace("../assets/images/", "")}`
-            }
-            width={1000}
-            height={1000}
-            style={{ objectFit: "cover" }}
-            className={`object-cover w-full h-[300px] ${!isFirst ? "md:h-[100px] " : "md:object-contain "}`}
-          />
-        </div>
-        </Link>
-
-    
-        <div
-          className={`${
-            isFirst ? "md:pl-6 md:w-[50%] text-left" : "md:text-left"
-          }`}
-        >
-          <div className={`cursor-pointer hover:text-pinkCustom dark:nightModePinkCustom transition-colors duration-300 `}>
-          <Link href={`/blog/${blogPost.blog_id}`}>
-          {isFirst && pathname !== "/" && 
-          <div className="text-xl text-pinkCustom md:block bg-gradient-to-r from-pinkCustom via-nightModeBlueCustom to-pinkCustom bg-[length:200%_100%] animate-gradient bg-clip-text text-transparent"><i>[Latest post]</i></div>
-        }
-            <div className={`text-3xl ${!isFirst && "md:text-2xl"}`}>
-              {blogPost.title}
-            </div>
-            <div className="text-xl pb-2">{blogPost.subtitle}</div>
-            <div className="text-base pb-5">{blogPost.excerpt}</div>
-          </Link>
-
-          */
